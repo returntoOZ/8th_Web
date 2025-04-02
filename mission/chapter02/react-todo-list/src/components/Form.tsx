fc75a65c66546a344f924a4f91c392efc0009dd3
returntooz <ojh3212@naver.com>
2025-04-02 21:38:33 +0900

    refactor: Form tag 내부의 불필요한 Fragment 제거

--- mission/chapter02/react-todo-list/src/components/Form.tsx
+++ mission/chapter02/react-todo-list/src/components/Form.tsx
@@ -2,28 +2,26 @@ import { ChangeEvent, FormEvent, useState } from "react";
 import { useDark } from "../context/darkProvider";
 
 interface FormProps {
-    onSubmit?: (e: FormEvent<HTMLFormElement>) => void;
-    input: string;
-    onChange?: (e: ChangeEvent<HTMLInputElement>) => void;
-    text: string;
+  onSubmit?: (e: FormEvent<HTMLFormElement>) => void;
+  input: string;
+  onChange?: (e: ChangeEvent<HTMLInputElement>) => void;
+  text: string;
 }
 
-const Form = ({onSubmit, input, onChange, text}: FormProps) => {
+const Form = ({ onSubmit, input, onChange, text }: FormProps) => {
   const { isDark } = useDark();
 
   return (
-    <>
-        <form onSubmit={onSubmit} className='todo-container__form' style={{backgroundColor: isDark? 'black' : 'white'}}>
-            <input value={input} 
-                onChange={onChange}
-                type='text'
-                className='todo-container__input'
-                placeholder='할 일 입력'
-                required 
-                style={{backgroundColor: isDark? '#3c3c3c' : 'white', color: isDark? 'white' : 'black'}}/>
-            <button type='submit' className='todo-container__button'>{text}</button>
-        </form>
-    </>
+    <form onSubmit={onSubmit} className='todo-container__form' style={{ backgroundColor: isDark ? 'black' : 'white' }}>
+      <input value={input}
+        onChange={onChange}
+        type='text'
+        className='todo-container__input'
+        placeholder='할 일 입력'
+        required
+        style={{ backgroundColor: isDark ? '#3c3c3c' : 'white', color: isDark ? 'white' : 'black' }} />
+      <button type='submit' className='todo-container__button'>{text}</button>
+    </form>
   )
 }
 
