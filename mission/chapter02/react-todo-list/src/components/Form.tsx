f67e2c3fc9cdcdd211962c49d3f05958e41b9d55
returntooz <ojh3212@naver.com>
2025-04-04 15:57:45 +0900

    refactor: SectionProps에서 불필요한 optional 프로퍼티 제거 및 별도의 함수로 분리(handleOnClick) & map 함수에서 todo 객체 비구조화 코드 추가

--- mission/chapter02/react-todo-list/src/components/Form.tsx
+++ mission/chapter02/react-todo-list/src/components/Form.tsx
@@ -1,4 +1,4 @@
-import { ChangeEvent, FormEvent, useState } from "react";
+import { ChangeEvent, FormEvent } from "react";
 import { useDark } from "../context/darkProvider";
 
 interface FormProps {
--- mission/chapter02/react-todo-list/src/components/Section.tsx
+++ mission/chapter02/react-todo-list/src/components/Section.tsx
@@ -4,27 +4,34 @@ import { TTodo } from "../types/todo";
 interface SectionProps {
     title: string;
     todos: TTodo[];
-    onClick?: (todo: TTodo) => void;
+    onClick: (todo: TTodo) => void;
     backgroundColor: string;
     button_name: string;
 };
 // Map 사용 시, Key prop을 전달해야함, 리액트가 Key를 기준으로 rendering이 이뤄지기 때문이다!
-const Section = ({title, todos, onClick, backgroundColor, button_name}: SectionProps) => {
+const Section = ({ title, todos, onClick, backgroundColor, button_name }: SectionProps) => {
     const { isDark } = useDark();
 
-  return (
-    <div className='render-container__section'>
-        <h2 className="render-container__title" style={{color: isDark? 'white' : 'black'}}>{title}</h2>
-        <ul id='todo-list' className="render-container__list">
-            {todos.map((todo) =>(
-                <li key={todo.id} className="render-container__item" style={{backgroundColor: isDark? '#3c3c3c' : '#f9f9f9'}}>
-                    <span className="render-container__item-text" style={{color: isDark? 'white' : 'black'}}>{todo.text}</span>
-                    <button onClick={() => onClick && onClick(todo)} style={{backgroundColor}} className="render-container__item-button">{button_name}</button>
-                </li>
-            ))}
-        </ul>
-    </div>
-  )
+    const handleOnClick = (todo: TTodo) => {
+        if (onClick) onClick(todo);
+    }
+
+    return (
+        <div className='render-container__section'>
+            <h2 className="render-container__title" style={{ color: isDark ? 'white' : 'black' }}>{title}</h2>
+            <ul id='todo-list' className="render-container__list">
+                {todos.map((todo) => {
+                    const { id, text } = todo;
+                    return (
+                        <li key={id} className="render-container__item" style={{ backgroundColor: isDark ? '#3c3c3c' : '#f9f9f9' }}>
+                            <span className="render-container__item-text" style={{ color: isDark ? 'white' : 'black' }}>{text}</span>
+                            <button onClick={() => handleOnClick(todo)} style={{ backgroundColor }} className="render-container__item-button">{button_name}</button>
+                        </li>
+                    )
+                })}
+            </ul>
+        </div>
+    )
 }
 
 export default Section
