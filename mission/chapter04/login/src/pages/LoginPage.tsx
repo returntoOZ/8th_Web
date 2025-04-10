dfcdfba4cf767af588d63e4ac4f79bda3bc448b3
returntooz <ojh3212@naver.com>
2025-04-10 12:40:44 +0900

    refactor: loginPage.tsx 코드 포메팅 적용

--- mission/chapter04/login/src/pages/LoginPage.tsx
+++ mission/chapter04/login/src/pages/LoginPage.tsx
@@ -6,71 +6,69 @@ import { useLocalStorage } from "../hooks/useLocalStorage";
 import { ResponseSigninDto } from "../types/auth";
 
 const LoginPage = () => {
-  const { setItem } = useLocalStorage(LOCAL_STORAGE_KEY.accessToken);
+    const { setItem } = useLocalStorage(LOCAL_STORAGE_KEY.accessToken);
 
-  const { values, errors, touched, getInputProps } = useForm<UserSigninInformation>({
-    initialValue: {
-      email: "",
-      password: "",
-    },
-    validate: validateSignin,
-  });
+    const { values, errors, touched, getInputProps } = useForm<UserSigninInformation>({
+        initialValue: {
+            email: "",
+            password: "",
+        },
+        validate: validateSignin,
+    });
 
-  const handleSubmit = async () => {
-    console.log("Submitted values: ", values);
-    try {
-      const response: ResponseSigninDto = await postSignin(values);
-      setItem(response.data.accessToken);
-      console.log("로그인 성공:", response);
-    } catch (error: any) {
-      console.error("로그인 실패", error);
-      alert(error?.message || "로그인 실패");
-    }
-  };
+    const handleSubmit = async () => {
+        console.log("Submitted values: ", values);
+        try {
+            const response: ResponseSigninDto = await postSignin(values);
+            setItem(response.data.accessToken);
+            console.log("로그인 성공:", response);
+        } catch (error: any) {
+            console.error("로그인 실패", error);
+            alert(error?.message || "로그인 실패");
+        }
+    };
 
-  // 오류가 하나라도 있거나, 입력값이 비어있으면 버튼을 비활성화
-  const isDisabled: boolean =
-    Object.values(errors || {}).some((error: string) => error.length > 0) ||
-    Object.values(values).some((value: string) => value === "");
+    // 오류가 하나라도 있거나, 입력값이 비어있으면 버튼을 비활성화
+    const isDisabled: boolean =
+        Object.values(errors || {}).some((error: string) => error.length > 0) ||
+        Object.values(values).some((value: string) => value === "");
 
-  return (
-    <div className="flex flex-col items-center justify-center h-full gap-4">
-      <div className="flex flex-col gap-3">
-        <input
-          {...getInputProps("email")}
-          className={`border border-[#ccc] w-[300px] p-[10px] focus:border-[#807bff] rounded-sm ${
-            errors?.email && touched?.email ? "border-red-500 bg-red-200" : "border-gray-300"
-          }`}
-          type="email"
-          placeholder="이메일"
-        />
-        {errors?.email && touched?.email && (
-          <div className="text-red-500 text-sm">{errors.email}</div>
-        )}
+    return (
+        <div className="flex flex-col items-center justify-center h-full gap-4">
+            <div className="flex flex-col gap-3">
+                <input
+                    {...getInputProps("email")}
+                    className={`border border-[#ccc] w-[300px] p-[10px] focus:border-[#807bff] rounded-sm ${errors?.email && touched?.email ? "border-red-500 bg-red-200" : "border-gray-300"
+                        }`}
+                    type="email"
+                    placeholder="이메일"
+                />
+                {errors?.email && touched?.email && (
+                    <div className="text-red-500 text-sm">{errors.email}</div>
+                )}
 
-        <input
-          {...getInputProps("password")}
-          className={`border border-[#ccc] w-[300px] p-[10px] focus:border-[#807bff] rounded-sm ${
-            errors?.password && touched?.password ? "border-red-500 bg-red-200" : "border-gray-300"
-          }`}
-          type="password"
-          placeholder="비밀번호"
-        />
-        {errors?.password && touched?.password && (
-          <div className="text-red-500 text-sm">{errors.password}</div>
-        )}
+                <input
+                    {...getInputProps("password")}
+                    className={`border border-[#ccc] w-[300px] p-[10px] focus:border-[#807bff] rounded-sm ${errors?.password && touched?.password ? "border-red-500 bg-red-200" : "border-gray-300"
+                        }`}
+                    type="password"
+                    placeholder="비밀번호"
+                />
+                {errors?.password && touched?.password && (
+                    <div className="text-red-500 text-sm">{errors.password}</div>
+                )}
 
-        <button
-          type="button"
-          onClick={handleSubmit}
-          disabled={isDisabled}
-          className="w-full bg-blue-600 text-white py-3 rounded-md text-lg font-medium hover:bg-blue-700 transition-colors cursor-pointer disabled:bg-gray-300"
-        >
-          로그인
-        </button>
-      </div>
-    </div>
-  );
+                <button
+                    type="button"
+                    onClick={handleSubmit}
+                    disabled={isDisabled}
+                    className="w-full bg-blue-600 text-white py-3 rounded-md text-lg font-medium hover:bg-blue-700 transition-colors cursor-pointer disabled:bg-gray-300"
+                >
+                    로그인
+                </button>
+            </div>
+        </div>
+    );
 };
 
 export default LoginPage;
